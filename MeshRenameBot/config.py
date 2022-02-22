6e69c486b28e9614bf6bc13c42c2a9fd97d42f0b
namutehani <58953928+namutehani@users.noreply.github.com>
2022-02-22 20:29:32 +0300

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -16,12 +16,12 @@ except ImportError:
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
+        AUTH_USERS = [list]
         OWNER_ID = [int, 0]
 
         # Public username url or invite link of private chat
         FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN_ID = [int]
 
         TRACE_CHANNEL = [int, 0]
 
